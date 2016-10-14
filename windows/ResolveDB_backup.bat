a4927044b17f276dcff565946d3aa34e4536672d
Walter Arrighetti <walter-arrighetti@users.noreply.github.com>
2016-10-14 02:55:55 +0200

    Create ResolveDB_backup.bat
    To be copied in C:\Users\Default\Documents (otherwise the Scheduled Tasks script must be edited accordingly).
    This dumps the whole PosgreSQL database to a local folder, then compresses it and moves the compressed version to a remote/shared/network/cloud path (or another form of more secure storage).
    Needs 7-Zip (64-bit) to be installed in its default directory for generate compressed versions of the database backups (on average ~8-11% of their uncompressed size).

---
+++ windows/ResolveDB_backup.bat
@@ -0,0 +1,21 @@
+@Echo OFF
+Echo.
+Echo **********************************************
+Echo **  PosgreSQL Backup script for Blackmagic  **
+Echo **  Design DaVinci Resolve Studio database  **
+Echo **==========================================**
+Echo **   Copyright (C) 2016 Walter Arrighetti   **
+Echo **********************************************
+Echo.
+Set ResolveDBpath=C:\Users\Default\Documents\
+Set ResolveDBnetpath=\\NAS\ResolveDB\
+dir \\10.0.0.100\ResolveDB\ >> dirL.out
+del dirL.out
+FOR /F %%a in ('WMIC Path Win32_LocalTime Get DayOfWeek /format:list ^| findstr "="') DO (set %%a)
+Echo Dumping all the PostgreSQL database for %DayOfWeek%th day of current week.....
+"C:\Program Files\PostgreSQL\9.2\bin\pg_dumpall.exe" --oids --username=postgres --file=%ResolveDBpath%\%DayOfWeek%.pgSQL
+Echo [  OK  ]
+Echo Generating a second (compressed) copy of this backup.....
+"C:\Program Files\7-Zip\7z.exe"  a  %ResolveDBpath%\%DayOfWeek%.7z  %ResolveDBpath%\%DayOfWeek%.pgSQL
+Move  /Y  %ResolveDBpath%\%DayOfWeek%.7z  %ResolveDBnetpath%
+Echo [  OK  ]
