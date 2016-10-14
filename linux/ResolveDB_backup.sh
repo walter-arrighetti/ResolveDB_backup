3adf8a23bbf828204ea8b1ddce4623f556773ade
Walter Arrighetti <walter-arrighetti@users.noreply.github.com>
2016-10-14 03:11:51 +0200

    Create ResolveDB_backup.sh
    Bash script to be copied in "/usr/local/bin" (please edit the cronjob creator script accordingly if different). It actually dumps all the local system's PostgreSQL database to a file on a local path, then compresses it and moves both copies to a remote folder.
    Default local folder is "/root"; default remote folder is "/mnt/NAS" and PostgreSQL binaries are expected to be in the Path environment variable.

---
+++ linux/ResolveDB_backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo.
+echo **********************************************
+echo **  PosgreSQL Backup script for Blackmagic  **
+echo **  Design DaVinci Resolve Studio database  **
+echo **==========================================**
+echo **   Copyright (C) 2016 Walter Arrighetti   **
+echo **********************************************
+echo.
+PGDumpAll=$(which pg_dumpall)
+ResolveDBpath=/root/
+ResolveDBnetpath=/mnt/NAS/
+DOM=$(date +%d)
+DOW=($($DOM % 7)) |sed 's/^0*//'
+echo Dumping all the PostgreSQL database for week day No.$DOW.....
+$PGDumpAll  --oids  --username=postgres  --file=$ResolveDBpath/$DOW.pgSQL
+echo [  OK  ]
+echo Generating a second (compressed) copy of this backup.....
+gzip  $ResolveDBpath/$DOW.pgSQL
+rsync  -Pav  $ResolveDBpath/$DOW.*  $ResolveDBnetpath/
