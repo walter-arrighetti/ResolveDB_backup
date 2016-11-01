7640861076255c1de564318d16b53fbb1573ba13
Walter Arrighetti <walter-arrighetti@users.noreply.github.com>
2016-11-01 11:39:08 +0100

    Update and rename ResolveDB-backup-mkcronjob.sh to ResolveDB-backup-install.sh

--- linux/ResolveDB-backup-install.sh
+++ linux/ResolveDB-backup-install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cp ResolveDB_backup.sh /usr/local/bin/
+chmod 775 /usr/local/bin/ResolveDB_backup.sh
 crontab -l |{ cat; echo "0 2 * * * /usr/local/bin/ResolveDB_backup.sh"; } |crontab -
